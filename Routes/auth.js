274fcbc87560194df801d3722a794061b02f2900
samiullah239 <2014tabontech@gmail.com>
2024-09-09 02:12:23 -0700

    done

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import { signIn,signUp,updateUser,logout,newSignUp,updateUserInShopify} from '../controller/auth.js'
 
-import multer  from 'multer';
-const upload = multer({ dest: 'uploads/' });
+
 
 const authRouter =express.Router()
 authRouter.post('/signIn',signIn)
@@ -11,5 +10,5 @@ authRouter.put('/update/:shopifyId',updateUser)
 authRouter.post('/logout',logout)
 authRouter.post('/newSignUp',newSignUp)
 authRouter.put('/updateInShopify',updateUserInShopify)
-// authRouter.put('/metafield/:shopifyId',upload.single('profileImage'),updateMetaFeild)
+
 export default authRouter;
