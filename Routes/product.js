a1af889b6b11f0a2c3f5a90d9bfe16cd02d6fc2f
samiullah239 <2014tabontech@gmail.com>
2024-09-09 02:57:42 -0700

    done

--- Routes/product.js
+++
@@ -1,9 +0,0 @@
-import { fetchAndStoreProducts ,addProduct,updateProduct, handleShopifyWebhook} from "../controller/product.js";
-import upload from "../middlleware/upload.js";
-import express from 'express'
-const productRouter=express.Router()
-productRouter.get('/shopify',fetchAndStoreProducts)
-productRouter.post('/addProduct',upload.single('image'),addProduct)
-productRouter.put('/update/:id',upload.single('image'),updateProduct)
-productRouter.post('/webhook',handleShopifyWebhook )
-export default productRouter
