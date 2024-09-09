c9f7d05ddc591a9c6794a90f16bdbe87c6908388
samiullah239 <2014tabontech@gmail.com>
2024-09-09 04:53:04 -0700

    done

--- connection/connect.js
+++ connection/connect.js
@@ -14,10 +14,27 @@ const Connect = () => {
   mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-  
+    serverSelectionTimeoutMS: 50000, // Optional: increase timeout
+  });
+
+  mongoose.connection.on('connected', () => {
+    console.log(connected('MongoDB connected'));
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error(error('MongoDB connection error:'), err);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log(disconnected('MongoDB disconnected'));
+  });
+
+  // Graceful shutdown
+  process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log(disconnected('MongoDB disconnected due to app termination'));
+    process.exit(0);
+  });
 };
 
 export default Connect;
