85049d23c5507033bf062dd9bd7d5e0ad0729c3e
samiullah239 <2014tabontech@gmail.com>
2024-09-21 03:55:38 -0700

    new changes

--- controller/order.js
+++ controller/order.js
@@ -99,15 +99,29 @@ const fetchProductIds = async () => {
 
 
 
+
+
+
+
+
+
+
 export const createOrder = async (req, res) => {
     const orderData = req.body;
-
     console.log("Incoming order data:", JSON.stringify(orderData, null, 2));
 
+    const { customer_email, customerName, line_items } = orderData;
+
     // Validate required fields
-    const { shipping_address, line_items, id } = orderData;
-    if (!shipping_address || !Array.isArray(line_items) || line_items.length === 0 || !id) {
-        return res.status(400).send({ message: 'Shipping address, line items, and order ID are required' });
+    if (!customerName || !customer_email || !Array.isArray(line_items) || line_items.length === 0 || !orderData.id) {
+        return res.status(400).send({ message: 'Customer name, email, line items, and order ID are required' });
+    }
+
+    // Check if line items have correct structure
+    for (const item of line_items) {
+        if (!item.product_id || typeof item.quantity !== 'number') {
+            return res.status(400).send({ message: 'Each line item must have a valid product_id and quantity' });
+        }
     }
 
     try {
@@ -151,138 +165,49 @@ export const createOrder = async (req, res) => {
         }
 
         const totalAmount = validItems.reduce((total, item) => total + item.price * item.quantity, 0);
-        const shippingCost = parseFloat(orderData.shipping_lines[0]?.price || 0);
-        const grandTotal = totalAmount + shippingCost;
 
         const newOrder = new orderModel({
-            id: orderData.id,
-            shipping_address,
-            total_amount: grandTotal.toFixed(2),
+            orderId: orderData.id.toString(),
+            customerEmail: customer_email,
+            customerName: customerName,
             items: validItems,
-            created_at: new Date(),
-            shipping_lines: orderData.shipping_lines,
-            // Additional fields can be added as necessary
+            totalAmount,
+        });
+
+        newOrder.subscriptionEndDate = new Date();
+        validItems.forEach(item => {
+            if (item.quantity > 0) {
+                newOrder.subscriptionEndDate.setMonth(newOrder.subscriptionEndDate.getMonth() + item.quantity);
+            }
         });
 
         await newOrder.save();
 
+        for (const item of validItems) {
+            const product = await productModel.findOne({ shopifyId: item.productId });
+            if (product) {
+                product.inventory_quantity -= item.quantity;
+                product.status = product.inventory_quantity > 0 ? 'active' : 'inactive';
+                if (product.inventory_quantity === 0) {
+                    product.subscriptionEndDate = null;
+                }
+                await product.save();
+            }
+        }
+
         res.status(201).send({
             message: 'Order saved successfully',
-            orderId: newOrder.id,
-            totalAmount: grandTotal,
+            orderId: newOrder.orderId,
+            createdAt: newOrder.createdAt,
+            subscriptionEndDate: newOrder.subscriptionEndDate,
+            totalAmount: totalAmount,
             items: validItems,
-            shipping_address: newOrder.shipping_address,
         });
     } catch (error) {
         console.error('Error saving order:', error);
         res.status(500).send({ message: 'Error saving order', error: error.message });
     }
 };
-
-
-
-// export const createOrder = async (req, res) => {
-//     const orderData = req.body;
-//     console.log("Incoming order data:", JSON.stringify(orderData, null, 2));
-
-//     const { customer_email, customerName, line_items } = orderData;
-
-//     // Validate required fields
-//     if (!customerName || !customer_email || !Array.isArray(line_items) || line_items.length === 0 || !orderData.id) {
-//         return res.status(400).send({ message: 'Customer name, email, line items, and order ID are required' });
-//     }
-
-//     // Check if line items have correct structure
-//     for (const item of line_items) {
-//         if (!item.product_id || typeof item.quantity !== 'number') {
-//             return res.status(400).send({ message: 'Each line item must have a valid product_id and quantity' });
-//         }
-//     }
-
-//     try {
-//         const productIds = line_items.map(item => item.product_id.toString());
-//         const validItems = [];
-        
-//         const shopifyApiKey = process.env.SHOPIFY_API_KEY;
-//         const shopifyPassword = process.env.SHOPIFY_ACCESS_TOKEN;
-//         const shopifyStore = process.env.SHOPIFY_STORE_URL;
-
-//         for (const productId of productIds) {
-//             try {
-//                 const response = await fetch(`https://${shopifyStore}/admin/api/2023-04/products/${productId}.json`, {
-//                     method: 'GET',
-//                     headers: {
-//                         'Authorization': `Basic ${Buffer.from(`${shopifyApiKey}:${shopifyPassword}`).toString('base64')}`,
-//                     }
-//                 });
-
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     if (data.product) {
-//                         const item = line_items.find(item => item.product_id.toString() === productId);
-//                         validItems.push({
-//                             productId: productId,
-//                             name: data.product.title,
-//                             quantity: item.quantity,
-//                             price: parseFloat(item.price),
-//                         });
-//                     }
-//                 } else {
-//                     console.error(`Product ID ${productId} not found: ${response.statusText}`);
-//                 }
-//             } catch (error) {
-//                 console.error(`Error fetching product ID ${productId}: ${error.message}`);
-//             }
-//         }
-
-//         if (validItems.length === 0) {
-//             return res.status(400).send({ message: 'No valid product IDs found in Shopify' });
-//         }
-
-//         const totalAmount = validItems.reduce((total, item) => total + item.price * item.quantity, 0);
-
-//         const newOrder = new orderModel({
-//             orderId: orderData.id.toString(),
-//             customerEmail: customer_email,
-//             customerName: customerName,
-//             items: validItems,
-//             totalAmount,
-//         });
-
-//         newOrder.subscriptionEndDate = new Date();
-//         validItems.forEach(item => {
-//             if (item.quantity > 0) {
-//                 newOrder.subscriptionEndDate.setMonth(newOrder.subscriptionEndDate.getMonth() + item.quantity);
-//             }
-//         });
-
-//         await newOrder.save();
-
-//         for (const item of validItems) {
-//             const product = await productModel.findOne({ shopifyId: item.productId });
-//             if (product) {
-//                 product.inventory_quantity -= item.quantity;
-//                 product.status = product.inventory_quantity > 0 ? 'active' : 'inactive';
-//                 if (product.inventory_quantity === 0) {
-//                     product.subscriptionEndDate = null;
-//                 }
-//                 await product.save();
-//             }
-//         }
-
-//         res.status(201).send({
-//             message: 'Order saved successfully',
-//             orderId: newOrder.orderId,
-//             createdAt: newOrder.createdAt,
-//             subscriptionEndDate: newOrder.subscriptionEndDate,
-//             totalAmount: totalAmount,
-//             items: validItems,
-//         });
-//     } catch (error) {
-//         console.error('Error saving order:', error);
-//         res.status(500).send({ message: 'Error saving order', error: error.message });
-//     }
-// };
 
 
 export const getOrderById = async (req, res) => {
