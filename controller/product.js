d9d954d5c79f4dcbc474f9cf4f86fddc14994fce
samiullah239 <2014tabontech@gmail.com>
2024-10-14 23:20:16 -0700

    new changes

--- controller/product.js
+++ controller/product.js
@@ -3006,7 +3006,7 @@ export const updateListing = async (req, res) => {
       product: {
         title: `${updateData.name || updateData.qualificationRequested} | ${country} , ${updateData.location} , ${updateData.zip}`,
         body_html: updateData.description,
-        vendor: updateData.brand,
+        vendor: updateData.brand || updateData.location,
         tags: `zip_${updateData.zip}, location_${updateData.location}, username_${username}`,
         images: product.images, // Attach updated images
       },
