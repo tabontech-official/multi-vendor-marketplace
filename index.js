1101676d9c33971dfc5696d4a65f4545703ae40e
samiullah239 <2014tabontech@gmail.com>
2024-12-04 05:25:29 -0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import app from './app.js';
 
 const port = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.setTimeout(300000, () => {  // Timeout after 5 minutes (300000ms)
+    res.status(504).send('Request timed out');
+  });
+  next();
+});
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
