68d568d560ee17cb57a65e1d746261028aa008ed
samiullah239 <2014tabontech@gmail.com>
2024-12-09 22:37:37 -0700

    done

--- Routes/product.js
+++ Routes/product.js
@@ -22,7 +22,8 @@ import {
   fetchPricePerCredit,
   fetchRequireCredits,
   lookingFor,
-  getAllProductData
+  getAllProductData,
+  uploadImage
 } from '../controller/product.js';
 import {  cpUpload } from '../middleware/cloudinary.js';
 import { verifyShopifyWebhook } from '../middleware/verifyShopifyWebhook.js';
@@ -53,4 +54,5 @@ productRouter.delete('/', deletAllProduct);
 productRouter.put('/credits', updateCredits);
 productRouter.get('/fetchRequireCredits',fetchRequireCredits)
 productRouter.post('/lookingFor',cpUpload,lookingFor)
+productRouter.post('/uploadImages',cpUpload,uploadImage)
 export default productRouter;
