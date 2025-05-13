042be284b8729febee61f9e9360414e53d61144b
Bism@786@786 <2014tabontech@gmail.com>
2025-05-13 11:29:12 +0500

    INVENTORY PAGE COMPLETED

--- Routes/apiCredentials.js
+++ Routes/apiCredentials.js
@@ -1,8 +1,8 @@
-import { generateAcessKeys } from "../controller/apicredential.js";
+import { generateAcessKeys, getApiCredentialByUserId } from "../controller/apicredential.js";
 import express from 'express'
 
 const apiCredentialsRouter=express.Router()
 apiCredentialsRouter.post('/generate-keys',generateAcessKeys)
-
+apiCredentialsRouter.get('/getApiCredentialByUserId/:userId',getApiCredentialByUserId)
 
 export default apiCredentialsRouter;
--- controller/apicredential.js
+++ controller/apicredential.js
@@ -1,5 +1,6 @@
 import { apiCredentialModel } from "../Models/apicredential.js";
 import crypto from 'crypto';
+import mongoose from "mongoose";
 
 const generateApiKey = () => `shpka_${crypto.randomBytes(16).toString('hex')}`;
 const generateApiSecretKey = () => `shpsk_${crypto.randomBytes(16).toString('hex')}`;
@@ -28,5 +29,40 @@ export const generateAcessKeys = async (req, res) => {
     res.json({ apiKey, apiSecretKey });
   } catch (err) {
     res.status(500).json({ error: 'Failed to generate credentials' });
+  }
+};
+
+
+export const getApiCredentialByUserId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ error: 'Invalid user ID format' });
+    }
+
+    const result = await apiCredentialModel.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(userId)
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          apiKey: 1,
+          apiSecretKey: 1
+        }
+      }
+    ]);
+
+    if (!result.length) {
+      return res.status(404).json({ message: 'No API credentials found for this user' });
+    }
+
+    res.status(200).json(result[0]);
+  } catch (error) {
+    console.error('Error fetching API credentials:', error);
+    res.status(500).json({ error: 'Server error' });
   }
 };
